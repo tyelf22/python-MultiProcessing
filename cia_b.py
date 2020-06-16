d74720ca39c93e0e377406c01c5f648462fac8fa
tyelf22 <tyelf22@hotmail.com>
2020-06-15 20:44:22 -0600

    added comments

--- cia_b.py
+++ cia_b.py
@@ -1,26 +1,37 @@
-import requests as rq 
-import time
-import concurrent.futures
+''' Tyson Elfors
+6/15/20
+CS-1410
+Project 7 - Concurrent Programming
+'''
 
-start = time.perf_counter()
+"""I declare that the following source code was written solely by me.
+I understand that copying any source code, in whole or in part, constitues cheating,
+and that I will receive a zero on this project if I am found in violation of this policy."""
 
-with open('flags.txt') as f:
-    allFlags = [line.strip() for line in f]
+import requests as rq #requests module
+import time #time module
+import concurrent.futures #multi-processing module
 
-byteSize = 0
+start = time.perf_counter() #start time
+
+with open('flags.txt') as f: #open flags file 
+    allFlags = [line.strip() for line in f] #read each line of flags file and add to list
+
+byteSize = 0 #initialize byte size
 
 def flagFunc(flag):
-    r = rq.get(f'https://www.cia.gov/library/publications/resources/the-world-factbook/graphics/flags/large/{flag}-lgflag.gif')
-    with open(f'./flags/{flag}.png', 'wb') as f:
-        f.write(r.content)
+    '''Download country flag images using requests module'''
+    r = rq.get(f'https://www.cia.gov/library/publications/resources/the-world-factbook/graphics/flags/large/{flag}-lgflag.gif') #get url based on flag
+    with open(f'./flags/{flag}.png', 'wb') as f: #put flag image in flags folder
+        f.write(r.content) #write bytes to file
     global byteSize
-    byteSize += len(r.content)
+    byteSize += len(r.content) #add to byteSize variable
 
-with concurrent.futures.ProcessPoolExecutor() as executor:
-    executor.map(flagFunc, allFlags)
+with concurrent.futures.ProcessPoolExecutor() as executor: #use multi-processing
+    executor.map(flagFunc, allFlags) #use multi-processing to map through flagFunc
 
 
-finish = time.perf_counter()
+finish = time.perf_counter() #end time
 
 with open("cia_b_results.pdf", 'a') as f: #open text file for appending
             print(f'Elapsed Time: {round(finish-start, 2)} \n{byteSize} bytes downloaded', file=f) #print to text file
